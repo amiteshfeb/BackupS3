8bd5249340ca3186a91a740343dad741e569f393
Amitesh Gupta <amiteshfeb@gmail.com>
2019-07-18 16:29:12 +0530

    New VC

--- vc-replace.sh
+++ vc-replace.sh
@@ -1,6 +1,6 @@
 cd public_html/ditelcomm/wp-content/plugins/
 rm -rf js_composer/
 rm -rf js_composer.zip
-wget https://github.com/amiteshfeb/ServerScripts/blob/master/js_composer.zip?raw=true
+wget https://github.com/amiteshfeb/ServerScripts/blob/master/js_composer.zip
 unzip js_composer.zip
 rm -rf js_composer.zip
