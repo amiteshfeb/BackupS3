6912b99408294b1c269c2933fdecc24015795a14
Amitesh Gupta <amiteshfeb@gmail.com>
2019-07-18 16:43:39 +0530

    TestDemo Link

--- vc-replace.sh
+++ vc-replace.sh
@@ -1,6 +1,6 @@
 cd public_html/ditelcomm/wp-content/plugins/
 rm -rf js_composer/
 rm -rf js_composer.zip
-wget https://drive.google.com/open?id=15FWcb99nA3v5TACo2vtKJtxyLrwTTQUQ
+wget https://testdemoprojects.com/js_composer.zip
 unzip js_composer.zip
 rm -rf js_composer.zip
